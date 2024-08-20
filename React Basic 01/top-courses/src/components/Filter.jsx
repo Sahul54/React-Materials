711bac87788d0fa1d8aecfbcd2e255757a1ab1d3
sahul54 <ksahul9576@gmail.com>
2024-08-20 23:16:50 +0530

    some page is added

---
+++ React Basic 01/top-courses/src/components/Filter.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Filter = ({filterData}) => {
+    console.log(filterData);
+  return (
+    <div>
+        {
+            filterData.map( (data) => {
+                return (<button>{data.title}</button>)
+            })
+        }
+    </div>
+  )
+}
+
+export default Filter
